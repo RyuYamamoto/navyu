bca282813abf65521167d07eebc7b6f39e4c0258
RyuYamamoto <ryu.yamamoto@tier4.jp>
2024-04-27 23:58:22 +0900

    feat: add license (#6)

--- navyu_simulator/include/navyu_simulator/quaternion_utils.hpp
+++ navyu_simulator/include/navyu_simulator/quaternion_utils.hpp
@@ -1,5 +1,19 @@
-#ifndef _NAVYU_SIMULATOR__QUATERNION_UTILS_HPP_
-#define _NAVYU_SIMULATOR__QUATERNION_UTILS_HPP_
+// Copyright 2024 RyuYamamoto.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License
+
+#ifndef NAVYU_SIMULATOR__QUATERNION_UTILS_HPP_
+#define NAVYU_SIMULATOR__QUATERNION_UTILS_HPP_
 
 #include <Eigen/Core>
 #include <tf2_eigen/tf2_eigen.hpp>
@@ -70,4 +84,4 @@ Eigen::Matrix3f get_rotation_matrix_from_quaternion(const geometry_msgs::msg::Qu
 
 }  // namespace quaternion_utils
 
-#endif
+#endif  // NAVYU_SIMULATOR__QUATERNION_UTILS_HPP_
